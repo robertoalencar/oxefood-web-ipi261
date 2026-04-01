deaef1af5254d9e13163608d736e21df43b7bc54
Roberto Alencar <rroberto.alencar@gmail.com>
2026-04-01 16:15:56 -0300

    atualizando projeto

--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -1,8 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 
+import FormCategoriaProduto from "./views/categoriaproduto/FormCategoriaProduto";
+import ListCategoriaProduto from "./views/categoriaproduto/ListCategoriaProduto";
 import FormCliente from './views/cliente/FormCliente';
 import ListCliente from './views/cliente/ListCliente';
 import Home from './views/home/Home';
+import FormProduto from "./views/produto/FormProduto";
+import ListProduto from "./views/produto/ListProduto";
 
 function Rotas() {
     return (
@@ -11,6 +15,12 @@ function Rotas() {
                 <Route path="/" element={ <Home/> } />
                 <Route path="form-cliente" element={ <FormCliente/> } />
                 <Route path="list-cliente" element={ <ListCliente/> } />
+
+                <Route path="form-categoria" element={ <FormCategoriaProduto/> } />
+                <Route path="list-categoria" element={ <ListCategoriaProduto/> } />
+
+                <Route path="form-produto" element={ <FormProduto/> } />
+                <Route path="list-produto" element={ <ListProduto/> } />
             </Routes>
         </>
     )
